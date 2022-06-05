7e9f4c5d83878130423a9b33b88677d433ac2d1e
serbeld <serbeldiaz@gmail.com>
2022-06-05 15:28:40 -0500

    Adding Styles

---
+++ src/App/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import './files/styles-menu.css';
+import './files/styles-product.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="overlay">
+        <div className="content">
+          <span className="close-icon">x</span>
+          <div className="wrap-input">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

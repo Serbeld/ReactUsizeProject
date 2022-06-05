c3a3e637947259ccff7654ab92a72709d3c4b1de
serbeld <serbeldiaz@gmail.com>
2022-06-05 16:32:22 -0500

    Update ClassName on Usize Form

--- src/UsizeForm/index.js
+++ src/UsizeForm/index.js
@@ -2,27 +2,27 @@ function UsizeForm() {
     return (
         <div className="React-Form">
 
-            <form class="form">
+            <form className="form">
 
-                <div class="form__section">
-                    <input type="espalda" class="form__input" placeholder="Ancho de espalda" required />
+                <div className="form__section">
+                    <input type="espalda" className="form__input" placeholder="Ancho de espalda" required />
                     <span>cm</span>
                 </div>
 
-                <div class="form__section">
-                    <input type="altura" class="form__input" placeholder="Altura" required />
+                <div className="form__section">
+                    <input type="altura" className="form__input" placeholder="Altura" required />
                     <span>cm</span>
                 </div>
 
-                <div class="form__section">
-                    <input type="peso" class="form__input" placeholder="Peso" required />
+                <div className="form__section">
+                    <input type="peso" className="form__input" placeholder="Peso" required />
                     <span>kg</span>
                 </div>
 
                 <br />
 
-                <div class="form__section">
-                    <button type="submit" class="form__input" >
+                <div className="form__section">
+                    <button type="submit" className="form__input" >
                         Predecir mi talla
                     </button>
                 </div>

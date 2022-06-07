f28fb8d21ea379a9959e19a63e31503368116cc7
serbeld <serbeldiaz@gmail.com>
2022-06-06 21:59:45 -0500

    Add Predictor Of Sizes Using Tensorflow

--- src/TrainingModel/index.js
+++ src/TrainingModel/index.js
@@ -11,17 +11,29 @@ import $ from "jquery"
 // datos para entrenar
 //datos de entrada 
 const datosEntra = [    [41, 157, 56],
-                        [47, 157, 56],
+                        [47, 174, 56],
                         [48, 157, 56],
                         [49, 157, 56],
-                        [41, 170, 65],
+                        [41, 174, 65],
                         [47, 170, 65],
                         [48, 170, 65],
                         [49, 170, 65],
                         [41, 183, 72],
                         [47, 183, 72],
+                        [48, 174, 72],
+                        [49, 183, 72],
+                        [41, 157, 56],
+                        [47, 157, 56],
+                        [48, 157, 56],
+                        [49, 174, 56],
+                        [41, 170, 65],
+                        [47, 174, 65],
+                        [48, 170, 65],
+                        [49, 170, 65],
+                        [41, 174, 72],
+                        [47, 183, 72],
                         [48, 183, 72],
-                        [49, 183, 72]];
+                        [49, 174, 72]];
 
 //Normalize the data
 var x = [];
@@ -35,24 +47,24 @@ const inputTensor = tf.tensor(x, [x.length, 3]);
 // console.log(datosEntrada)
 
 // resultados esperados para cada dato de entrada
-const datosEsp = [      ['S'],
-                        ['M'],
-                        ['L'],
-                        ['XL'],
-                        ['S'],
-                        ['M'],
-                        ['L'],
-                        ['XL'],
-                        ['S'],
-                        ['M'],
-                        ['L'],
-                        ['XL']];
+// const datosEsp = [      ['S'],
+//                         ['M'],
+//                         ['L'],
+//                         ['XL'],
+//                         ['S'],
+//                         ['M'],
+//                         ['L'],
+//                         ['XL'],
+//                         ['S'],
+//                         ['M'],
+//                         ['L'],
+//                         ['XL']];
 
-var y = [];
+// var y = [];
 
-datosEsp.map((talla) => (
-    y.push([(talla[0].charCodeAt(0))/100])
-));
+// datosEsp.map((talla) => (
+//     y.push([(talla[0].charCodeAt(0))/100])
+// ));
 
 // [S,M,L,XL]
 const categoricalY = [
@@ -68,6 +80,18 @@ const categoricalY = [
     [0,1,0,0],
     [0,0,1,0],
     [0,0,0,1],
+    [1,0,0,0],
+    [0,1,0,0],
+    [0,0,1,0],
+    [0,0,0,1],
+    [1,0,0,0],
+    [0,1,0,0],
+    [0,0,1,0],
+    [0,0,0,1],
+    [1,0,0,0],
+    [0,1,0,0],
+    [0,0,1,0],
+    [0,0,0,1]
 ]
 
 const outputTensor = tf.tensor(categoricalY, [categoricalY.length, 4]);
@@ -75,9 +99,9 @@ const outputTensor = tf.tensor(categoricalY, [categoricalY.length, 4]);
 // console.log(datosEsperados)
 const model = tf.sequential({
     layers: [
-        tf.layers.dense({ inputShape: [3], units: 10, activation: 'relu' }),
         tf.layers.dense({ inputShape: [3], units: 100, activation: 'relu' }),
-        tf.layers.dense({ inputShape: [3], units: 10, activation: 'relu' }),
+        tf.layers.dense({ units: 1000, activation: 'relu' }),
+        tf.layers.dense({ units: 100, activation: 'relu' }),
         tf.layers.dense({ units: 4, activation: 'softmax' }),
     ]
 });
@@ -91,21 +115,30 @@ model.compile({
 });
 
 async function learnSize() {
-    $(".info-training").append("<div class='title'>Start Training: </div>");
     model.fit(inputTensor,outputTensor, {
-        epochs: 1001,        
+        epochs: 201,        
         shuffle: true,
         callbacks: {
             onEpochEnd: async (epoch, { loss }) => {
-                if(epoch % 200 === 0){
+                if(epoch === 0){
+                    $(".info-training").append("<div class='title'>Start Training: </div>");
+                }
+                if(epoch % 50 === 0){
                     $(".info-training").append("<div class='epoch'> epoch: "+ epoch + " loss: " + loss + "</div>");
                 }
                 await tf.nextFrame();
             }
         }
-    }).then((info) => {   
-        $(".info-training").append("<div  class='epoch title'>Final Accuracy: " + info.history.acc[info.history.acc.length-1]*100 + "%</div><br/>");
-        model.save('localstorage://my-model-thm-size');
+    }).then(async (info) => {   
+        $(".info-training").append("<div  class='epoch title'>Final Accuracy: " + Math.round(info.history.acc[info.history.acc.length-1]*100 * 100) / 100 + "%</div><br/>");
+        // $(".info-training").append("<br/>");
+        await model.save('localstorage://my-model-thm-size');
+        // await model.save('downloads://my-model-thm-size');
+        // let predictions = (model.predict(tf.tensor([48/200,183/250,72/250], [1, 3])));
+        // predictions.print();
+        // let prediction = [] ;
+        // predictions.dataSync().forEach(predictedValue => prediction.push(Math.round(predictedValue * 100) / 100));
+        // console.log(prediction);
     });
 }
 
--- src/UsizeForm/index.js
+++ src/UsizeForm/index.js
@@ -1,49 +1,128 @@
 // npm install @tensorflow/tfjs
-// import * as tf from '@tensorflow/tfjs';
+import * as tf from '@tensorflow/tfjs';
+import React, { useState } from 'react';
+import svg from './up-arrow.min.svg';
+import './UsizeForm.css'
 
-// var i = 0;
+var talla = "S";
 
-// async function TestSize() {
-//     const inputData = tf.tensor2d([43,174,70], [1, 3]);
-//     const val = model(inputData);
-//     const myVal = await val.data()
-//     console.log(myVal)
+async function predict(event, setRenderForm) {
+    event.preventDefault();
+    try {
+        let model = await tf.loadLayersModel('localstorage://my-model-thm-size');
+        let espalda = event.target.elements["espalda"].value
+        let altura = event.target.elements["altura"].value
+        let peso = event.target.elements["peso"].value
 
-//     document.getElementById('divPrueba').innerHTML = '<div class="talla-ideal"><div class="talla-ideal-text">Tu talla ideal es:</div><div id="talla-ideal"></div></div>'
-// }
+        let predictions = (model.predict(tf.tensor([espalda / 200, altura / 250, peso / 250], [1, 3])));
 
-function UsizeForm() {
+        let prediction = [];
+        predictions.dataSync().forEach(predictedValue => prediction.push(Math.round(predictedValue * 100) / 100));
+
+        const max = Math.max(...prediction);
+
+        switch (prediction.indexOf(max)) {
+            case 0:
+                talla = "S"
+                setRenderForm(false);
+                break;
+            case 1:
+                talla = "M"
+                setRenderForm(false);
+                break;
+            case 2:
+                talla = "L"
+                setRenderForm(false);
+                break;
+            case 3:
+                talla = "XL"
+                setRenderForm(false);
+                break;
+            default:
+                break;
+        }
+
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+function ShowTalla({setRender}) {
+
     return (
-        <div className="React-Form">
-
-            <form className="form">
-
-                <div className="form__section">
-                    <input type="espalda" className="form__input" placeholder="Ancho de espalda" required />
-                    <span>cm</span>
+        <div className='show-talla-predicted'>
+            <div className='info-talla-predicted'>
+                <div className='title-sugestion'>Tu talla sugerida es:</div>
+                <div class="talla-predicted-text-container"> 
+                    <div className='talla-predicted-text'>
+                        {talla} 
+                    </div>
                 </div>
-
-                <div className="form__section">
-                    <input type="altura" className="form__input" placeholder="Altura" required />
-                    <span>cm</span>
+                <div class="predicted-bar-container">
+                    <span class="left-title">
+                        Ajustado
+                    </span>
+                    <span class="middle-title">A medida</span>
+                    <span class="right-title">
+                        Holgado
+                    </span>
+                    <div class="talla-predicted-result-line">
+                        <img class="talla-predicted-result-marker" src={svg} alt="arrow" width="16px" height="16px" />
+                        <div class="talla-predicted-result-red talla-predicted-result-left"></div>
+                        <div class="talla-predicted-result-green"></div>
+                        <div class="talla-predicted-result-red talla-predicted-result-right"></div>
+                    </div>
                 </div>
-
-                <div className="form__section">
-                    <input type="peso" className="form__input" placeholder="Peso" required />
-                    <span>kg</span>
-                </div>
-
-                <br />
-
-                <div className="form__section">
-                    <button type="submit" className="form__input" >
-                        Predecir mi talla
-                    </button>
-                </div>
-
-            </form>
+            </div>
+            <div className='come-back'>
+                <button className='come-back-button' onClick={() => {setRender()}}>
+                    Volver atras
+                </button>
+            </div>
         </div>
     );
+}
+
+function UsizeForm() {
+
+    const [renderForm, setRenderForm] = useState(true);
+
+    const setTrue = () => {
+        setRenderForm(true);
+    };
+    
+    if (renderForm === true) {
+        return (
+            <div className="React-Form">
+                <form className="form" onSubmit={(event) => { predict(event, setRenderForm) }} >
+                    <div className="form__section">
+                        <input type="number" name="espalda" className="form__input espalda" placeholder="Ancho de espalda" required />
+                        <span>cm</span>
+                    </div>
+                    <div className="form__section">
+                        <input type="number" name="altura" className="form__input altura" placeholder="Altura" required />
+                        <span>cm</span>
+                    </div>
+                    <div className="form__section">
+                        <input type="number" name="peso" className="form__input peso" placeholder="Peso" required />
+                        <span>kg</span>
+                    </div>
+                    <br />
+                    <div className="form__section">
+                        <button type="submit" className="form__input" >
+                            Predecir mi talla
+                        </button>
+                    </div>
+                </form>
+            </div>
+        );
+    } else {
+        return (
+            <div className="React-Form">
+                <ShowTalla setRender={setTrue} />
+            </div>
+        );
+    }
 }
 
 export default UsizeForm;
